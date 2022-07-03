7c5dca9e50ccbc9628028cd53b0d46dfa5c945dc
Steve B <wkdatabase@gmail.com>
2022-07-03 21:51:33 +0200

    app setup

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+mongoose.connect('mongodb+srv://wkdvie:020290Ab@cluster0.f1b0g.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+  })
+);
 
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
 
-// mongoDB-connect:
-const mongoDB = process.env.NODE_ENV_MONGODB;
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
